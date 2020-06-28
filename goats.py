fed3398540430eb82507d0bb006ddd7bcf2b98be
root <aleksandar.i.uzunov@gmail.com>
2020-06-28 23:13:14 +0300

    Fixed the goats task

--- goats.py
+++ goats.py
@@ -1,18 +1,29 @@
-import itertools
-
-goatsnum=int(input("Enter the number of goats: "))
-courses=int(input("Enter the number of courses: "))
-goats=[]
+#Козички - Александър Узунов
+inputdata = input().split(" ")
+goatsnum= int(inputdata[0])
+courses=int(inputdata[1])
+inputdata2= input().split(" ")
+transported = [None] * goatsnum
+goats = []
 for i in range(goatsnum):
-	goats.append(int(input("Enter the weight of goat #{} ".format(i+1))))
-totalgoatweight = sum(goats)
-goats.sort(reverse=True) 
-finalboatweight=-1
-i=0
-while i < goatsnum:
-	for x in itertools.permutations(goats, i):
-		if sum(x)==totalgoatweight/courses:
-			if(finalboatweight<sum(x)):
-				finalboatweight=sum(x)
-	i=i+1
-print("The boat weight must be: {:d}".format(finalboatweight))
+	goats.append(int(inputdata2[i]))
+goats.sort()
+boatsize = goats[len(goats)-1]
+flag = 1
+while flag==1:
+	for i in range(0,goatsnum):
+		transported[i] = 0
+	flag=0
+	boatsize=boatsize+1
+	for i in range(0,courses):
+		currboatsize = 0
+		for j in range(goatsnum-1,-1,-1):
+			if currboatsize < boatsize:
+				if transported[j]==0 and currboatsize+goats[j]<=boatsize:
+					currboatsize = currboatsize + goats[j]
+					transported[j]=1
+	for i in range(0,goatsnum):
+		if transported[i]!=1:
+			flag=1
+			break
+print(boatsize)
