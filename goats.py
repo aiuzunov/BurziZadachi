1712693a0d8c572ae32df07f1c8a2c921b17c98d
root <aleksandar.i.uzunov@gmail.com>
2020-06-29 23:41:28 +0300

    Added Input Validation

--- goats.py
+++ goats.py
@@ -1,12 +1,23 @@
 #Козички - Александър Узунов
+print("Input N (Number of goats) and K (Number of courses)")
 inputdata = input().split(" ")
 goatsnum= int(inputdata[0])
 courses=int(inputdata[1])
+while goatsnum<1 or goatsnum >1000:
+	print("Please input a valid value for N:")
+	goatsnum = int(input())
+while courses<1 or courses >1000:
+	print("Please input a valid value for K:")
+	courses = int(input())
+print("Input the weights of the goats")
 inputdata2= input().split(" ")
 transported = [None] * goatsnum
 goats = []
 for i in range(goatsnum):
 	goats.append(int(inputdata2[i]))
+	while goats[i]<0 or goats[i] > 100000:
+		print("Please input a valid weight for goat #{:d}:".format(i+1))
+		goats[i]=int(input()) 
 goats.sort()
 boatsize = goats[len(goats)-1]
 flag = 1
